9b4ae206badc2c6e923b67ec88e388bdb07fc8b6
Qirui Sun <qiruisun@gmail.com>
2016-04-08 14:13:37 -0400

    Initialize a small application using react

---
+++ simple_application1/app.js
@@ -0,0 +1,33 @@
+
+(function () {
+    'use strict';
+
+    var Quiz = React.createClass({
+      propTypes: {
+          books: React.PropTypes.array.isRequired
+      },
+      render: function () {
+          return <div>
+              {this.props.books.map(function (b) {
+                  return <Book title={b} />;
+              })}
+            </div>;
+      }
+    });
+
+    var Book = React.createClass({
+      propTypes: {
+          title: React.PropTypes.string.isRequired
+      },
+      render: function () {
+          return <div>
+                    <h4> {this.props.title} </h4>
+                </div>;
+      }
+    })
+
+    ReactDOM.render(
+      <Quiz books={['The Lord of the Rings', 'The Unkonwn']} />,
+      document.getElementById('app')
+    );
+})();
