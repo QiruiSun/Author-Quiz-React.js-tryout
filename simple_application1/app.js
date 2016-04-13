57a6b0b43d8b920bd4e031f94ad440c6c340ff50
Qirui Sun <qiruisun@gmail.com>
2016-04-13 17:47:16 -0400

    Add routie.js for client-server routing and create a url with # for add game form

--- simple_application1/app.js
+++ simple_application1/app.js
@@ -19,6 +19,9 @@
               showContinue: isCorrect
           });
       },
+      handleAddGame: function () {
+          routie('add');
+      },
       handleContinue: function () {
           this.setState(this.getInitialState());
       },
@@ -43,6 +46,11 @@
                   </div>
                 ) : <span/>
               }
+               <div className="row">
+                  <div className="col-md-12">
+                      <input onClick={this.handleAddGame} id="addGameBtn" type="button" value="Add Game" className="bth btn-primary" />
+                  </div>
+               </div>
             </div>
             );
       }
@@ -59,6 +67,34 @@
           return <div onClick={this.handleClick} className="answer">
                     <h4> {this.props.title} </h4>
                 </div>;
+      }
+    });
+
+    var AddGameForm = React.createClass({
+      render: function () {
+          return (<div className="row">
+                    <div className="col-md-12">
+                      <h1>Add Game</h1>
+                      <form role="form" onSubmit={this.handleSubmit}>
+                          <div className="form-group">
+                              <input ref="imageUrl" type="text" className="form-control" placeholder="Image Url" />
+                          </div>
+                          <div className="form-group">
+                              <input ref="answer1" type="text" className="form-control" placeholder="Answer 1" />
+                          </div>
+                          <div className="form-group">
+                              <input ref="answer2" type="text" className="form-control" placeholder="Answer 2" />
+                          </div>
+                          <div className="form-group">
+                              <input ref="answer3" type="text" className="form-control" placeholder="Answer 3" />
+                          </div>
+                          <div className="form-group">
+                              <input ref="answer4" type="text" className="form-control" placeholder="Answer 4" />
+                          </div>
+                          <button type="submit" className="btn btn-default"> Submit </button>
+                      </form>
+                    </div>
+                </div>);
       }
     });
 
@@ -135,9 +171,19 @@
       };
     }
 
+    routie({
+        " ": function () {
+          ReactDOM.render(
+            <Quiz data={data} />,
+            document.getElementById('app')
+          );
+        },
+        "add": function () {
+          ReactDOM.render(
+            <AddGameForm/>, document.getElementById('app')
+          )
+        }
 
-    ReactDOM.render(
-      <Quiz data={data} />,
-      document.getElementById('app')
-    );
+    });
+
 })();
